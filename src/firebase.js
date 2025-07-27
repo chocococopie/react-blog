1b5ba2fe3ede1f09a6faba0613f23cea3135cb82
Diana Shagiakhmetova <deadeluna@dianas-air.speedport.ip>
2025-07-27 14:19:30 +0200

    chore: set up Firebase Hosting and deploy initial build

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwFXSy80HS5snW33YU4ccaAbvCn15Lp38",
+  authDomain: "react-n-node-blog.firebaseapp.com",
+  projectId: "react-n-node-blog",
+  storageBucket: "react-n-node-blog.firebasestorage.app",
+  messagingSenderId: "858136500551",
+  appId: "1:858136500551:web:aa10ed6bdacf8505449462"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
